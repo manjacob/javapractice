fec51d6ad2cede4992569c30cc3de015553c2244
Manoj Jacob <manjacob@gmail.com>
2017-08-06 17:11:10 -0400

    initial commit

---
+++ MyThing.java
@@ -0,0 +1,18 @@
+
+public class MyThing
+{
+  
+  public static void main (String []args){
+    System.out.println("Hello me!");
+    int a = 9;
+    int b =10;
+    
+    double c = 9.87;
+    float d = 9.87f;
+    boolean e = false;
+    String f = "Grant";
+    
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
